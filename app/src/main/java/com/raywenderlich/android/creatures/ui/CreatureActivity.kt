9677b53b3a4a243b0eb0b09e66b3ac87d15ecb02
Mislav Kostic <mislav.kostic@gmail.com>
2022-03-22 08:59:14 +0100

    Implemented Foods horizontal RecyclerView

--- app/src/main/java/com/raywenderlich/android/creatures/ui/CreatureActivity.kt
+++ app/src/main/java/com/raywenderlich/android/creatures/ui/CreatureActivity.kt
@@ -35,6 +35,8 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.raywenderlich.android.creatures.R
 import com.raywenderlich.android.creatures.model.Favorites
 import com.raywenderlich.android.creatures.model.Creature
@@ -43,71 +45,81 @@ import kotlinx.android.synthetic.main.activity_creature.*
 
 class CreatureActivity : AppCompatActivity() {
 
-  private lateinit var creature: Creature
+    private lateinit var creature: Creature
+    private val adapter = FoodAdapter(mutableListOf())
 
-  companion object {
-    private const val EXTRA_CREATURE_ID = "EXTRA_CREATURE_ID"
+    companion object {
+        private const val EXTRA_CREATURE_ID = "EXTRA_CREATURE_ID"
 
-    fun newIntent(context: Context, creatureId: Int): Intent {
-      val intent = Intent(context, CreatureActivity::class.java)
-      intent.putExtra(EXTRA_CREATURE_ID, creatureId)
-      return intent
+        fun newIntent(context: Context, creatureId: Int): Intent {
+            val intent = Intent(context, CreatureActivity::class.java)
+            intent.putExtra(EXTRA_CREATURE_ID, creatureId)
+            return intent
+        }
     }
-  }
 
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_creature)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_creature)
 
-    setupCreature()
-    setupTitle()
-    setupViews()
-    setupFavoriteButton()
-  }
-
-  private fun setupCreature() {
-    val creatureById = CreatureStore.getCreatureById(intent.getIntExtra(EXTRA_CREATURE_ID, 1))
-    if (creatureById == null) {
-      Toast.makeText(this, getString(R.string.invalid_creature), Toast.LENGTH_SHORT).show()
-      finish()
-    } else {
-      creature = creatureById
+        setupCreature()
+        setupTitle()
+        setupViews()
+        setupFavoriteButton()
+        setupFoods()
     }
-  }
 
-  private fun setupTitle() {
-    title = String.format(getString(R.string.detail_title_format), creature.nickname)
-    supportActionBar?.setDisplayHomeAsUpEnabled(true)
-  }
-
-  private fun setupViews() {
-    headerImage.setImageResource(resources.getIdentifier(creature.uri, null, packageName))
-    fullName.text = creature.fullName
-    planet.text = creature.planet
-  }
-
-  private fun setupFavoriteButton() {
-    setupFavoriteButtonImage(creature)
-    setupFavoriteButtonClickListener(creature)
-  }
-
-  private fun setupFavoriteButtonImage(creature: Creature) {
-    if (creature.isFavorite) {
-      favoriteButton.setImageDrawable(getDrawable(R.drawable.ic_favorite_black_24dp))
-    } else {
-      favoriteButton.setImageDrawable(getDrawable(R.drawable.ic_favorite_border_black_24dp))
+    private fun setupCreature() {
+        val creatureById = CreatureStore.getCreatureById(intent.getIntExtra(EXTRA_CREATURE_ID, 1))
+        if (creatureById == null) {
+            Toast.makeText(this, getString(R.string.invalid_creature), Toast.LENGTH_SHORT).show()
+            finish()
+        } else {
+            creature = creatureById
+        }
     }
-  }
 
-  private fun setupFavoriteButtonClickListener(creature: Creature) {
-    favoriteButton.setOnClickListener { _ ->
-      if (creature.isFavorite) {
-        favoriteButton.setImageDrawable(getDrawable(R.drawable.ic_favorite_border_black_24dp))
-        Favorites.removeFavorite(creature, this)
-      } else {
-        favoriteButton.setImageDrawable(getDrawable(R.drawable.ic_favorite_black_24dp))
-        Favorites.addFavorite(creature, this)
-      }
+    private fun setupTitle() {
+        title = String.format(getString(R.string.detail_title_format), creature.nickname)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
-  }
+
+    private fun setupViews() {
+        headerImage.setImageResource(resources.getIdentifier(creature.uri, null, packageName))
+        fullName.text = creature.fullName
+        planet.text = creature.planet
+    }
+
+    private fun setupFavoriteButton() {
+        setupFavoriteButtonImage(creature)
+        setupFavoriteButtonClickListener(creature)
+    }
+
+    private fun setupFavoriteButtonImage(creature: Creature) {
+        if (creature.isFavorite) {
+            favoriteButton.setImageDrawable(getDrawable(R.drawable.ic_favorite_black_24dp))
+        } else {
+            favoriteButton.setImageDrawable(getDrawable(R.drawable.ic_favorite_border_black_24dp))
+        }
+    }
+
+    private fun setupFavoriteButtonClickListener(creature: Creature) {
+        favoriteButton.setOnClickListener { _ ->
+            if (creature.isFavorite) {
+                favoriteButton.setImageDrawable(getDrawable(R.drawable.ic_favorite_border_black_24dp))
+                Favorites.removeFavorite(creature, this)
+            } else {
+                favoriteButton.setImageDrawable(getDrawable(R.drawable.ic_favorite_black_24dp))
+                Favorites.addFavorite(creature, this)
+            }
+        }
+    }
+
+    private fun setupFoods() {
+        foodRecyclerView.layoutManager =
+            LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        foodRecyclerView.adapter = adapter
+        val foods = CreatureStore.getCreatureFoods(creature)
+        adapter.updateFoods(foods)
+    }
 }
