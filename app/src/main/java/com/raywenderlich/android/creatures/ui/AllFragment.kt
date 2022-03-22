1bed2a99dba034ff17cabc0a27e94437cd26b06c
Mislav Kostic <mislav.kostic@gmail.com>
2022-03-22 10:36:31 +0100

    added menu for switching from list to grid layout

--- app/src/main/java/com/raywenderlich/android/creatures/ui/AllFragment.kt
+++ app/src/main/java/com/raywenderlich/android/creatures/ui/AllFragment.kt
@@ -31,10 +31,8 @@
 package com.raywenderlich.android.creatures.ui
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
@@ -46,11 +44,22 @@ import kotlinx.android.synthetic.main.fragment_all.*
 class AllFragment : Fragment() {
 
     private val adapter = CreatureCardAdapter(CreatureStore.getCreatures().toMutableList())
+    private lateinit var layoutManager: StaggeredGridLayoutManager
 
     companion object {
         fun newInstance(): AllFragment {
             return AllFragment()
         }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.menu_all, menu)
     }
 
     override fun onCreateView(
@@ -63,9 +72,32 @@ class AllFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
+        layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
 
         creatureRecyclerView.layoutManager = layoutManager
         creatureRecyclerView.adapter = adapter
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+        when (id) {
+            R.id.action_span_1 -> {
+                showListView()
+                return true
+            }
+            R.id.action_span_2 -> {
+                showGridView()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun showListView(){
+        layoutManager.spanCount = 1
+    }
+
+    private fun showGridView(){
+        layoutManager.spanCount = 2
     }
 }
